ebcf693ab5af89f01676bd71cf9592cf981d67ce
claudiocro <claudiocro@gmail.com>
2012-11-07 11:28:41 +0100

    removed own model as we use ember-data from now on

--- test/p8emberjs-ds_test.js
+++ test/p8emberjs-ds_test.js
@@ -22,68 +22,6 @@
       raises(block, [expected], [message])
   */
 
-  module('P8DS.Model');
-  
-  var P8TESTDS = Ember.Namespace.create();
-  window.P8TESTDS = P8TESTDS;
-
-  P8TESTDS.PersonModel = P8DS.Model.extend({
-    properties : [ 'id', 'name', 'lastname', 'date'],
-    fullname:function() {
-      return this.get('name') + " " + this.get('lastname'); 
-    }.property('name', 'firstname').cacheable()
-  });
-  
-  
-  P8TESTDS.PersonGroupModel = P8DS.Model.extend({
-    properties : [ 'tags[]', 'persons[P8TESTDS.PersonModel]', 'fake', 'chief{P8TESTDS.PersonModel}']
-  });
-  
-  
-  test('create', 15, function() {
-
-    var personGroupModel = P8TESTDS.PersonGroupModel.create();
-    personGroupModel.updateFrom({"tags":['tagA', 'tagB'],'persons':[
-      {"name":"Hans","lastname":"Muster", "date":"2012-12-11 00:00:00","fake":"nope"},
-      {"name":"Beppe","lastname":"Rossi", "date":"2012-01-10 00:00:00"}],
-      "chief":{"name":"Beppea","lastname":"Rossia", "date":"2012-01-15 00:00:00"}
-    });
-    var personModel = P8TESTDS.PersonModel.create({"ID":"1","name":"Hans","lastname":"Muster", "date":"2012-12-11 00:00:00", "fake":"nope"});
-    
-    equal(personModel.get('name'), "Hans", 'get name');
-    equal(personModel.get('lastname'), "Muster", 'get lastname');
-    equal(personModel.get('fullname'), "Hans Muster", 'get fullname');
-    equal(personModel.get('fake'), "nope", 'not defined property');
-    ok(personGroupModel.get('chief') instanceof P8TESTDS.PersonModel, 'child object instance of P8TESTDS.PersonModel');
-    equal(personGroupModel.get('chief.lastname'), "Rossia", 'child object lastname');
-    
-    ok(personGroupModel.get('persons') instanceof Ember.ArrayProxy, 'objectarray instance of Ember.ArrayProxy');
-    equal(personGroupModel.get('persons.length'), 2, 'objectarray length');
-    ok(personGroupModel.get('persons').objectAt(0) instanceof P8TESTDS.PersonModel, 'objectarray instance of member');
-    equal(personGroupModel.get('persons').objectAt(1).get('fullname'), "Beppe Rossi", 'objectarray object member compare');
-    equal(personGroupModel.get('persons').objectAt(1).get('fake'), undefined, 'objectarray object member compare undefined');
-    
-    ok(personGroupModel.get('tags') instanceof Ember.ArrayProxy, 'array instance of Ember.ArrayProxy');
-    equal(personGroupModel.get('tags.length'), 2, 'array length');
-    equal(personGroupModel.get('tags').objectAt(1), "tagB", 'objectarray object member compare');
-    
-    
-    ok(personGroupModel.get('persons') instanceof Ember.ArrayProxy, 'objectarray instance of Ember.ArrayProxy');
-  });
-  
-  test('appendProperty', 2, function() {
-    P8TESTDS.PersonModel.reopen({
-      additionalPropMyTags: "myTags[]"
-    });
-    var personModel = P8TESTDS.PersonModel.create({"ID":"1","name":"Hans","lastname":"Muster", "date":"2012-12-11 00:00:00"});
-    
-    equal(personModel.get('myTags.length'), 0);
-    
-    personModel.get('myTags').addObject('a');
-    equal(personModel.get('myTags.length'), 1);
-    
-  });
-
   module('P8DS.Store');
   test('put-get', 4, function() {
     var store = P8DS.Store.create({});
